5cd692247b18ff25e1441b7632e2eb2b06108a7a
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-01-08 21:04:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,8 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-function preload()
-{
-	
-}
 
 function setup() {
 	createCanvas(800, 700);
@@ -26,7 +23,11 @@ function setup() {
 	bob4 = new Bob(450, 400, 25);
 	bob5 = new Bob(500, 400, 25);
 
-	//rope1 = new Rope (bob1.body, roof.body)
+	rope1 = new Rope (bob1.body, roof.body, -100, 25);
+	rope2 = new Rope (bob2.body, roof.body, -50, 25);
+	rope3 = new Rope (bob3.body, roof.body, 0, 25);
+	rope4 = new Rope (bob4.body, roof.body, 50, 25);
+	rope5 = new Rope (bob5.body, roof.body, 100, 25);
 
 	Engine.run(engine);
   
@@ -43,10 +44,20 @@ function draw() {
   bob3.display();
   bob4.display();
   bob5.display();
+  
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
 
   drawSprites();
  
 }
 
-
-
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	  Matter.Body.setStatic(bob1.body, false);
+		Matter.Body.applyForce(bob1.body, bob1.body.position, {x:-30, y:-30})
+	}
+  }
