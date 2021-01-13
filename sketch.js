f4c00521b7ae8767934c6b57ab138fa6279e892a
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-01-13 09:52:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,18 +16,19 @@ function setup() {
 	//Bodies are created here
 
 	roof = new Roof(400, 100, 350, 40);
+	bobDiameter = 25;
 
-	bob1 = new Bob(300, 400, 25);
-	bob2 = new Bob(350, 400, 25);
-	bob3 = new Bob(400, 400, 25);
-	bob4 = new Bob(450, 400, 25);
-	bob5 = new Bob(500, 400, 25);
+	bob1 = new Bob(300, 400, bobDiameter);
+	bob2 = new Bob(350, 400, bobDiameter);
+	bob3 = new Bob(400, 400, bobDiameter);
+	bob4 = new Bob(450, 400, bobDiameter);
+	bob5 = new Bob(500, 400, bobDiameter);
 
-	rope1 = new Rope (bob1.body, roof.body, -100, 25);
-	rope2 = new Rope (bob2.body, roof.body, -50, 25);
-	rope3 = new Rope (bob3.body, roof.body, 0, 25);
-	rope4 = new Rope (bob4.body, roof.body, 50, 25);
-	rope5 = new Rope (bob5.body, roof.body, 100, 25);
+	rope1 = new Rope (bob1.body, roof.body, -(bobDiameter*4), bobDiameter);
+	rope2 = new Rope (bob2.body, roof.body, -(bobDiameter*2), bobDiameter);
+	rope3 = new Rope (bob3.body, roof.body, 0, bobDiameter);
+	rope4 = new Rope (bob4.body, roof.body, (bobDiameter*2), bobDiameter);
+	rope5 = new Rope (bob5.body, roof.body, (bobDiameter*4), bobDiameter);
 
 	Engine.run(engine);
   
@@ -57,7 +58,7 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-	  Matter.Body.setStatic(bob1.body, false);
-		Matter.Body.applyForce(bob1.body, bob1.body.position, {x:-30, y:-30})
+	  //Matter.Body.setStatic(bob1.body, false);
+		Matter.Body.applyForce(bob1.body, bob1.body.position, {x:-70, y:-65})
 	}
   }
